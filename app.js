c1008cf333d2f3c4802774c541e8011dc4792438
Toni <sancheztoni.garcia@gmail.com>
2020-05-21 21:10:55 +0200

    charcuteria

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
+const charcuteriaRouter = require("./routes/charcuteriaRouter");
+//const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use("/charcuteria", charcuteriaRouter);
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
